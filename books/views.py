eed5228e859585949e6509e03749eab7a46b6ab3
Joy Chen <joythewizard@gmail.com>
2017-04-29 20:38:02 +0000

    views: Change return view to redirection

--- books/views.py
+++ books/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.conf import settings
 from django.contrib.auth.models import User, AnonymousUser
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
@@ -39,7 +39,7 @@ def add_book(request):
             book = form.save(commit=False)
             book.listed_by = request.user
             book.save()
-            return index(request)
+            return redirect(index)
         else:
             print(form.errors)
     return render(request, 'books/add_book.html', {'form': form})
@@ -47,7 +47,7 @@ def add_book(request):
 def my_books(request):
     user = request.user
     if user.is_anonymous():
-        return index(request)
+        return redirect(index)
     books = Book.objects.filter(listed_by=user)
     context_dict = {'books': books}
     return render(request, 'books/my_books.html', context_dict)
@@ -60,7 +60,7 @@ def edit_book(request, book_id):
             if form.is_valid():
                 book = form.save(commit=False)
                 book.save()
-                return my_books(request)
+                return redirect(my_books)
         else:
             form = BookForm(instance=book)
     else:
@@ -71,7 +71,7 @@ def delete_book(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
     if request.user==book.listed_by:
         book.delete()
-    return index(request)
+    return redirect(my_books)
 
 def contact_seller(request, book_id):
     book = get_object_or_404(Book, pk=book_id)
@@ -84,6 +84,6 @@ def contact_seller(request, book_id):
           "HC Book Swap <%s>" % settings.DEFAULT_FROM_EMAIL, [seller.email])
         success = True
     else:
-        return index(request)
+        return redirect(index)
     context_dict = {'success': success}
     return render(request, 'books/contact_success.html', context_dict)
