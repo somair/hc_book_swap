b9176ad1be584aa687951e1f82a59dab193e211e
Joy Chen <joythewizard@gmail.com>
2017-05-05 19:43:32 +0000

    admin: Customize appearance

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
 from books.models import Book, Subject, Course
 
-admin.site.register(Book)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'name', 'teacher',)
+    list_display_links = ('name',)
+    search_fields = ('name', 'teacher',)
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('listed_by', 'name', 'author', 'isbn', 'submitted', 'sold')
+    list_display_links = ('name',)
+    search_fields = ('name', 'author', 'isbn',)
+    list_per_page = 20
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Subject)
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
