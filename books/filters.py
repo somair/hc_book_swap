84ea034b72630bea3d3646eed78906d2bcf8923c
Joy Chen <joythewizard@gmail.com>
2017-05-09 19:42:07 +0000

    filters: Adjust styles and display

--- books/filters.py
+++ books/filters.py
@@ -1,19 +1,17 @@
 import django_filters
 from django.conf import settings
 from books.models import Book
+from django import forms
 
 class BookFilter(django_filters.FilterSet):
-    isbn = django_filters.CharFilter(max_length=20, label='ISBN')
-    condition = django_filters.MultipleChoiceFilter(choices=settings.CONDITION_CHOICES)
+    price = django_filters.RangeFilter(required=False)
+    isbn = django_filters.CharFilter(max_length=20, label='ISBN', required=False, lookup_expr='iexact')
+    condition = django_filters.MultipleChoiceFilter(choices=settings.CONDITION_CHOICES, widget=forms.CheckboxSelectMultiple, required=False)
+    name = django_filters.CharFilter(max_length=120, lookup_expr='icontains', required=False)
+    author = django_filters.CharFilter(max_length=120, lookup_expr='icontains', required=False)
     
     class Meta:
         model = Book
-        fields = {
-            'price': ['range'],
-            'name': ['contains'],
-            'condition': ['exact'],
-            'isbn': ['exact'],
-            'author': ['contains']
-        }
+        fields = ['name', 'price', 'condition', 'isbn', 'author']
         exclude = ['course', 'thumbnail', 'description', 'listed_by', 'sold', 'submitted']
         strict = 'IGNORE'
